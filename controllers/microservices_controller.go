8a1aee26b5015070f760133608977f44c31dbc33
Ruben Vargas <ruben.vp8510@gmail.com>
2022-09-09 02:51:45 -0500

    boostraping tempo operator project (#1)
    * boostraping tempo operator project
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    * remove bin dir and add it to gitignore
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    * remove license
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    * bump golang version to 1.19
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

---
+++ controllers/microservices_controller.go
@@ -0,0 +1,46 @@
+package controllers
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	tempov1alpha1 "github.com/os-observability/tempo-operator/api/v1alpha1"
+)
+
+// MicroservicesReconciler reconciles a Microservices object
+type MicroservicesReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=tempo.grafana.com,resources=microservices,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=tempo.grafana.com,resources=microservices/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=tempo.grafana.com,resources=microservices/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the Microservices object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
+func (r *MicroservicesReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *MicroservicesReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&tempov1alpha1.Microservices{}).
+		Complete(r)
+}
