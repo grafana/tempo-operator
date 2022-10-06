3530f1f1f3007d1984d8f106ebe9bb6e6564cc47
Ruben Vargas <ruben.vp8510@gmail.com>
2022-10-06 00:40:19 -0500

    remove object metadata from config
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- apis/config.tempo/v1alpha1/projectconfig_types.go
+++ apis/config.tempo/v1alpha1/projectconfig_types.go
@@ -13,8 +13,6 @@ type ProjectConfig struct {
 	metav1.TypeMeta `json:",inline"`
 	// ControllerManagerConfigurationSpec returns the contfigurations for controllers
 	cfg.ControllerManagerConfigurationSpec `json:",inline"`
-
-	metav1.ObjectMeta `json:"metadata,omitempty"`
 }
 
 func init() {
