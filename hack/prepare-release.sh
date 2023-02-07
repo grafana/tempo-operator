0866ba1718be69939d76aad601b7712ca445b594
Israel Blancas <iblancasa@gmail.com>
2023-02-07 13:33:33 +0100

    Remove reference to versions.txt

--- hack/prepare-release.sh
+++ hack/prepare-release.sh
@@ -10,8 +10,5 @@ echo "Updating the Tempo images version"
 sed -i "s~docker.io/grafana/tempo:.*~docker.io/grafana/tempo:${TEMPO_VERSION}~gi" config/manager/controller_manager_config.yaml
 sed -i "s~docker.io/grafana/tempo-query:.*~docker.io/grafana/tempo-query:${TEMPO_VERSION}~gi" config/manager/controller_manager_config.yaml
 
-echo "Updating the versions.txt file"
-sed -i "s~operator=.*~operator=${OPERATOR_VERSION}~gi" versions.txt
-
 echo "Generating the bundle"
 USER=os-observability VERSION=${OPERATOR_VERSION} USER=tempo-operator make bundle
