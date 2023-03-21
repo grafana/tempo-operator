565b759356ad9e0ed30a35a310ef1ab89facd1ee
Andreas Gerstmayr <agerstmayr@redhat.com>
2023-03-21 14:11:54 +0100

    Add operator metrics
    Adds the following new metrics:
    * tempooperator_build_info{goversion, revision, version}
    * tempostack_status_condition{stack_namespace, stack_name, condition, status}
    
    Related: #308
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- internal/status/status.go
+++ internal/status/status.go
@@ -2,13 +2,26 @@ package status
 
 import (
 	"context"
+	"strings"
 
 	dockerparser "github.com/novln/docker-parser"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/metrics"
 
 	"github.com/os-observability/tempo-operator/apis/tempo/v1alpha1"
 )
 
-// Refresh updates the status field with the tempo container image versions.
+var (
+	tempoStackStatusCondition = promauto.With(metrics.Registry).NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: "tempostack",
+		Name:      "status_condition",
+		Help:      "The status condition of a TempoStack instance.",
+	}, []string{"stack_namespace", "stack_name", "condition", "status"})
+)
+
+// Refresh updates the status field with the tempo container image versions and updates the tempostack_status_condition metric.
 func Refresh(ctx context.Context, k StatusClient, tempo v1alpha1.TempoStack, status *v1alpha1.TempoStackStatus) (bool, error) {
 
 	changed := tempo.DeepCopy()
@@ -26,6 +39,15 @@ func Refresh(ctx context.Context, k StatusClient, tempo v1alpha1.TempoStack, sta
 			return false, err
 		}
 		changed.Status.TempoQueryVersion = tempoQueryImage.Tag()
+	}
+
+	for _, cond := range status.Conditions {
+		var isActive float64
+		if cond.Status == metav1.ConditionTrue {
+			isActive = 1
+		}
+		status := strings.ToLower(string(cond.Status))
+		tempoStackStatusCondition.WithLabelValues(tempo.Namespace, tempo.Name, cond.Type, status).Set(isActive)
 	}
 
 	err = k.PatchStatus(ctx, changed, &tempo)
