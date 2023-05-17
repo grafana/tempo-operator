d868528f7bb044147e95b1306778b1f00650ed0f
Andreas Gerstmayr <agerstmayr@redhat.com>
2023-05-17 20:32:01 +0200

    Re-add logging setup
    Afaics it was removed in #303 by mistake.
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

---
+++ main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func TestSetupLogging(t *testing.T) {
+	prevStderr := os.Stderr
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	setupLogging()
+	log := log.FromContext(context.Background())
+	log = log.WithValues("tempo", "simplest")
+	log.V(1).Info("a test debug message")
+	log.Info("a test info message")
+	log.Error(errors.New("test error"), "a test error occurred")
+
+	err := w.Close()
+	require.NoError(t, err)
+	output, _ := io.ReadAll(r)
+	os.Stderr = prevStderr
+
+	require.Regexp(t, `{"level":"info","ts":".+","msg":"a test info message","tempo":"simplest"}`, string(output))
+	require.NotRegexp(t, `{"level":"debug",.+}`, string(output))
+	require.Regexp(t, `{"level":"error","ts":".+","msg":"a test error occurred","tempo":"simplest","error":"test error","stacktrace":`, string(output))
+}
