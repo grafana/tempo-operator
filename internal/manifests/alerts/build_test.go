f5989f04e0da125c48a53204b92d0613fd47617e
Ruben Vargas <ruben.vp8510@gmail.com>
2023-07-21 08:43:37 -0600

    fix alerts build test
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- internal/manifests/alerts/build_test.go
+++ internal/manifests/alerts/build_test.go
@@ -10,14 +10,16 @@ import (
 func TestBuildRules(t *testing.T) {
 	rulesSpec, err := build(Options{
 		RunbookURL: RunbookDefaultURL,
+		Namespace:  "default",
+		Cluster:    "test",
 	})
 
 	require.NoError(t, err)
 	assert.Len(t, rulesSpec.Groups, 2)
-	assert.Equal(t, "tempo_alerts", rulesSpec.Groups[0].Name)
+	assert.Equal(t, "tempo_alerts_test_default", rulesSpec.Groups[0].Name)
 	assert.Len(t, rulesSpec.Groups[0].Rules, 14)
 
-	assert.Equal(t, "tempo_rules", rulesSpec.Groups[1].Name)
+	assert.Equal(t, "tempo_rules_test_default", rulesSpec.Groups[1].Name)
 	assert.Len(t, rulesSpec.Groups[1].Rules, 6)
 
 }
