2c935acefc9d0abeeac50b859d73c233da2cc979
Pavol Loffay <p.loffay@gmail.com>
2023-08-23 16:09:51 +0200

    fix
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- apis/tempo/v1alpha1/webhook_suite_test.go
+++ apis/tempo/v1alpha1/webhook_suite_test.go
@@ -11,11 +11,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
-	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
-	//+kubebuilder:scaffold:imports
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -23,6 +19,10 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	//+kubebuilder:scaffold:imports
 
 	"github.com/grafana/tempo-operator/apis/config/v1alpha1"
 )
