dec70e5fb4b85f40a738ff82c0ef1c99eec7cc2b
rita.canavarro <rita.canavarro@farfetch.com>
2023-09-26 10:25:19 +0100

    [FEAT] Creation of datasource
    Signed-off-by: rita.canavarro <rita.canavarro@farfetch.com>

--- internal/manifests/operator/grafana/datasource.go
+++ internal/manifests/operator/grafana/datasource.go
@@ -3,10 +3,17 @@ package grafana
 import (
 	grafanav1 "github.com/grafana-operator/grafana-operator/v5/api/v1beta1"
 	configv1alpha1 "github.com/grafana/tempo-operator/apis/config/v1alpha1"
+	"github.com/grafana/tempo-operator/internal/manifests/manifestutils"
 )
 
 // Datasource creates a Datasource for Grafana Tempo.
 func Datasource(featureGates configv1alpha1.FeatureGates, namespace string) *grafanav1.GrafanaDatasource {
+	var tlsSkipVerify = true
+
+	if featureGates.OpenShift.ServiceCertsService{
+		tlsSkipVerify = false
+	}
+
 	return &grafanav1.GrafanaDatasource{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "tempo",
@@ -18,7 +25,8 @@ func Datasource(featureGates configv1alpha1.FeatureGates, namespace string) *gra
 				Name:  "Tempo",
 				Type: "tempo",
 				// TODO: Change URL
-				URL:  "http://tempo-tempo-gateway:8080",
+				URL:  featureGates.Observability.Datasources.TempoGatewayEndpoint,
+				JSONData: json.RawMessage(fmt.Sprintf(`{"tlsSkipVerify": %t}`, tlsSkipVerify)),
 			},
 			InstanceSelector: metav1.LabelSelector{
 				MatchLabels: map[string]string{
