c31bbe16d4cb867270176135f4152810db84d8e7
rita.canavarro <rita.canavarro@farfetch.com>
2023-12-07 16:21:31 +0000

    [FEAT] Update Build args
    Signed-off-by: rita.canavarro <rita.canavarro@farfetch.com>

--- internal/manifests/grafana/datasource.go
+++ internal/manifests/grafana/datasource.go
@@ -7,26 +7,28 @@ import (
 )
 
 // BuildGrafanaDatasource creates a Datasource for Grafana Tempo.
-func BuildGrafanaDatasource(string namespace, string name) (*grafanav1.GrafanaDatasource, error) {
+func BuildGrafanaDatasource(params manifestutils.Params) (*grafanav1.GrafanaDatasource, error) {
 	var tlsSkipVerify = true
 	var url = naming.Name(manifestutils.QueryFrontendComponentName, name)
+	var component = manifestutils.QueryFrontendComponentName
 
 	if params.Tempo.Spec.Template.Gateway.Enabled {
 		url := naming.Name(manifestutils.GatewayComponentName, name)
+		component := manifestutils.GatewayComponentName
 	}
 
 	return &grafanav1.GrafanaDatasource{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      naming.Name(component, name),
-			Namespace: namespace,,
-			Labels:    manifestutils.CommonLabels(name),
+			Name:      naming.Name(component, params.Tempo.Name),
+			Namespace: params.Tempo.Namespace,
+			Labels:    manifestutils.CommonLabels(params.Tempo.Name),
 		},
 		Spec: grafanav1.GrafanaDatasourceSpec{
 			DatasourceSpec: grafanav1.DatasourceSpec{
 				Access: "proxy",
-				Name:  name,
+				Name:  params.Tempo.Name,
 				Type: "tempo",
-				URL:  "http://"+url+namespace+".svc:"+manifestutils.PortHTTPServer,
+				URL:  fmt.Sprintf("https://%s:%d", naming.ServiceFqdn(params.Tempo.Namespace, params.Tempo.Name, manifestutils.QueryFrontendComponentName), manifestutils.PortHTTPServer),
 				JSONData: json.RawMessage(fmt.Sprintf(`{"tlsSkipVerify": %t}`, tlsSkipVerify)),
 			},
 			InstanceSelector: metav1.LabelSelector{
