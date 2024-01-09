0f490ddbdc1bbf366dc2e697c1d301f56fabfb96
Andreas Gerstmayr <agerstmayr@redhat.com>
2024-01-09 17:51:37 +0100

    add configmap tests
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

---
+++ internal/manifests/monolithic/configmap_test.go
@@ -0,0 +1,141 @@
+package monolithic
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"testing"
+
+	configv1alpha1 "github.com/grafana/tempo-operator/apis/config/v1alpha1"
+	"github.com/grafana/tempo-operator/apis/tempo/v1alpha1"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestBuildConfigMap(t *testing.T) {
+	opts := Options{
+		CtrlConfig: configv1alpha1.ProjectConfig{
+			DefaultImages: configv1alpha1.ImagesSpec{
+				Tempo: "docker.io/grafana/tempo:x.y.z",
+			},
+		},
+		Tempo: v1alpha1.TempoMonolithic{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "sample",
+				Namespace: "default",
+			},
+			Spec: v1alpha1.TempoMonolithicSpec{
+				Storage: v1alpha1.MonolithicStorageSpec{
+					Traces: v1alpha1.MonolithicTracesStorageSpec{
+						Backend: "memory",
+					},
+				},
+				Ingestion: &v1alpha1.MonolithicIngestionSpec{
+					OTLP: &v1alpha1.MonolithicIngestionOTLPSpec{
+						GRPC: &v1alpha1.MonolithicIngestionOTLPProtocolsGRPCSpec{
+							Enabled: true,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	cm, checksum, err := BuildConfigMap(opts)
+	require.NoError(t, err)
+	require.NotNil(t, cm.Data)
+	require.NotNil(t, cm.Data["tempo.yaml"])
+	require.Equal(t, fmt.Sprintf("%x", sha256.Sum256([]byte(cm.Data["tempo.yaml"]))), checksum)
+}
+
+func TestBuildConfig(t *testing.T) {
+	opts := Options{
+		CtrlConfig: configv1alpha1.ProjectConfig{
+			DefaultImages: configv1alpha1.ImagesSpec{
+				Tempo: "docker.io/grafana/tempo:x.y.z",
+			},
+		},
+		Tempo: v1alpha1.TempoMonolithic{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "sample",
+				Namespace: "default",
+			},
+			Spec: v1alpha1.TempoMonolithicSpec{},
+		},
+	}
+
+	tests := []struct {
+		name      string
+		storage   v1alpha1.MonolithicStorageSpec
+		ingestion *v1alpha1.MonolithicIngestionSpec
+		expected  string
+	}{
+		{
+			name: "memory storage",
+			storage: v1alpha1.MonolithicStorageSpec{
+				Traces: v1alpha1.MonolithicTracesStorageSpec{
+					Backend: "memory",
+				},
+			},
+			expected: `
+server:
+  http_listen_port: 3200
+storage:
+  trace:
+    backend: local
+    wal:
+      path: /var/tempo/wal
+    local:
+      path: /var/tempo/blocks
+distributor:
+  receivers:
+    otlp:
+      protocols: {}
+usage_report:
+  reporting_enabled: false
+`,
+		},
+		{
+			name: "PV storage with OTLP/gRPC and OTLP/HTTP",
+			storage: v1alpha1.MonolithicStorageSpec{
+				Traces: v1alpha1.MonolithicTracesStorageSpec{
+					Backend: "pv",
+				},
+			},
+			ingestion: &v1alpha1.MonolithicIngestionSpec{
+				OTLP: &v1alpha1.MonolithicIngestionOTLPSpec{
+					GRPC: &v1alpha1.MonolithicIngestionOTLPProtocolsGRPCSpec{
+						Enabled: true,
+					},
+				},
+			},
+			expected: `
+server:
+  http_listen_port: 3200
+storage:
+  trace:
+    backend: local
+    wal:
+      path: /var/tempo/wal
+    local:
+      path: /var/tempo/blocks
+distributor:
+  receivers:
+    otlp:
+      protocols:
+        grpc: {}
+usage_report:
+  reporting_enabled: false
+`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			opts.Tempo.Spec.Storage = test.storage
+			opts.Tempo.Spec.Ingestion = test.ingestion
+			cfg, err := buildTempoConfig(opts)
+			require.NoError(t, err)
+			require.YAMLEq(t, test.expected, string(cfg))
+		})
+	}
+}
