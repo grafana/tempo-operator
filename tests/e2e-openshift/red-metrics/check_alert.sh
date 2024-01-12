f228de2361bf45674b98b3a7fbbbdd385cda3272
Ishwar Kanse <ikanse@redhat.com>
2024-01-12 14:19:24 +0530

    Test Span RED metrics and alerts.

---
+++ tests/e2e-openshift/red-metrics/check_alert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Run the command and save its output
+output=$(oc -n openshift-monitoring exec alertmanager-main-0 -- amtool --alertmanager.url http://localhost:9093 alert query SpanREDFrontendAPIRequestLatency 2>&1)
+
+# Check if the command was successful
+if [ $? -ne 0 ]; then
+    echo "Error executing oc command: $output"
+    exit 1
+fi
+
+# Check if the alert is active
+if echo "$output" | grep -q "SpanREDFrontendAPIRequestLatency.*active"; then
+    echo "Alert SpanREDFrontendAPIRequestLatency is firing"
+    exit 0
+else
+    echo "Alert SpanREDFrontendAPIRequestLatency is not firing"
+    exit 1
+fi
