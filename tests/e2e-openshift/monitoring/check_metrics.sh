71c85edc37510d723c44843928e76c2a0207c346
Ishwar Kanse <ikanse@redhat.com>
2024-02-26 19:47:42 +0530

    Fix tests using OpenShift monitoring (#820)

--- tests/e2e-openshift/monitoring/check_metrics.sh
+++ tests/e2e-openshift/monitoring/check_metrics.sh
@@ -8,16 +8,19 @@ THANOS_QUERIER_HOST=$(oc get route thanos-querier -n openshift-monitoring -o jso
 metrics="tempo_request_duration_seconds_count tempo_request_duration_seconds_sum tempo_request_duration_seconds_bucket tempo_build_info tempo_ingester_bytes_received_total tempo_ingester_flush_failed_retries_total tempo_ingester_failed_flushes_total tempo_ring_members"
 
 for metric in $metrics; do
-  query="$metric"
+query="$metric"
+count=0
 
-  response=$(curl -k -H "Authorization: Bearer $TOKEN" -H "Content-type: application/json" "https://$THANOS_QUERIER_HOST/api/v1/query?query=$query")
+# Keep fetching and checking the metrics until metrics with value is present.
+while [[ $count -eq 0 ]]; do
+    response=$(curl -k -H "Authorization: Bearer $TOKEN" -H "Content-type: application/json" "https://$THANOS_QUERIER_HOST/api/v1/query?query=$query")
+    count=$(echo "$response" | jq -r '.data.result | length')
 
-  count=$(echo "$response" | jq -r '.data.result | length')
-
-  if [[ $count -eq 0 ]]; then
-    echo "No metric '$metric' with value present. Exiting with status 1."
-    exit 1
-  else
+    if [[ $count -eq 0 ]]; then
+    echo "No metric '$metric' with value present. Retrying..."
+    sleep 5  # Wait for 5 seconds before retrying
+    else
     echo "Metric '$metric' with value is present."
-  fi
+    fi
+  done
 done
