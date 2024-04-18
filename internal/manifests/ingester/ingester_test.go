e99eb604f90c2f14fcd1ca1cc3af8b7143760fd3
Andreas Gerstmayr <agerstmayr@redhat.com>
2024-04-18 15:41:15 +0200

    Update Kubernetes libraries to v0.29.4 (#883)
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- internal/manifests/ingester/ingester_test.go
+++ internal/manifests/ingester/ingester_test.go
@@ -195,7 +195,7 @@ func TestBuildIngester(t *testing.T) {
 					},
 					Spec: corev1.PersistentVolumeClaimSpec{
 						AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-						Resources: corev1.ResourceRequirements{
+						Resources: corev1.VolumeResourceRequirements{
 							Requests: corev1.ResourceList{
 								corev1.ResourceStorage: resource.MustParse("10Gi"),
 							},
