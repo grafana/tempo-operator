f47d4c610f0328a77af8887421f246b1221479fc
Ruben Vargas <ruben.vp8510@gmail.com>
2024-05-23 12:52:49 -0600

    fix nil bad access
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- apis/tempo/v1alpha1/tempomonolithic_defaults.go
+++ apis/tempo/v1alpha1/tempomonolithic_defaults.go
@@ -59,32 +59,33 @@ func (r *TempoMonolithic) Default(ctrlConfig configv1alpha1.ProjectConfig) {
 			Enabled: true,
 		}
 	}
-	if r.Spec.JaegerUI != nil && r.Spec.JaegerUI.Enabled &&
-		r.Spec.JaegerUI.Route != nil && r.Spec.JaegerUI.Route.Enabled {
+	if r.Spec.JaegerUI != nil && r.Spec.JaegerUI.Enabled {
 
-		if r.Spec.JaegerUI.Route.Termination == "" {
-			if r.Spec.Multitenancy.IsGatewayEnabled() && ctrlConfig.Gates.OpenShift.ServingCertsService {
-				// gateway uses TLS
-				r.Spec.JaegerUI.Route.Termination = TLSRouteTerminationTypePassthrough
-			} else {
-				r.Spec.JaegerUI.Route.Termination = TLSRouteTerminationTypeEdge
+		if r.Spec.JaegerUI.Route != nil && r.Spec.JaegerUI.Route.Enabled {
+
+			if r.Spec.JaegerUI.Route.Termination == "" {
+				if r.Spec.Multitenancy.IsGatewayEnabled() && ctrlConfig.Gates.OpenShift.ServingCertsService {
+					// gateway uses TLS
+					r.Spec.JaegerUI.Route.Termination = TLSRouteTerminationTypePassthrough
+				} else {
+					r.Spec.JaegerUI.Route.Termination = TLSRouteTerminationTypeEdge
+				}
 			}
-		}
 
-		if r.Spec.JaegerUI.Authentication == nil {
-			r.Spec.JaegerUI.Authentication = &JaegerQueryAuthenticationSpec{
-				Enabled: ctrlConfig.Gates.OpenShift.OauthProxy.DefaultEnabled,
+			if r.Spec.JaegerUI.Authentication == nil {
+				r.Spec.JaegerUI.Authentication = &JaegerQueryAuthenticationSpec{
+					Enabled: ctrlConfig.Gates.OpenShift.OauthProxy.DefaultEnabled,
+				}
 			}
-		}
 
-		if len(strings.TrimSpace(r.Spec.JaegerUI.Authentication.SAR)) == 0 {
-			defaultSAR := fmt.Sprintf("{\"namespace\": \"%s\", \"resource\": \"pods\", \"verb\": \"get\"}", r.Namespace)
-			r.Spec.JaegerUI.Authentication.SAR = defaultSAR
+			if len(strings.TrimSpace(r.Spec.JaegerUI.Authentication.SAR)) == 0 {
+				defaultSAR := fmt.Sprintf("{\"namespace\": \"%s\", \"resource\": \"pods\", \"verb\": \"get\"}", r.Namespace)
+				r.Spec.JaegerUI.Authentication.SAR = defaultSAR
+			}
 		}
 
 		if r.Spec.JaegerUI.ServicesQueryDuration == nil {
 			r.Spec.JaegerUI.ServicesQueryDuration = &defaultServicesDuration
 		}
-
 	}
 }
