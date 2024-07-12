b151cb3939220abb6656f3d2515445978e93223f
Ruben Vargas <ruben.vp8510@gmail.com>
2024-07-12 13:22:29 -0600

    Sanitize volume names for ca bundle and certificates
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>

--- internal/manifests/distributor/distributor.go
+++ internal/manifests/distributor/distributor.go
@@ -77,11 +77,12 @@ func resources(tempo v1alpha1.TempoStack) corev1.ResourceRequirements {
 func configureReceiversTLS(dep *v1.Deployment, caSecretName, certSecretName string) error {
 	podSpec := &dep.Spec.Template.Spec
 	if caSecretName != "" {
+		volumeName := naming.DNSName(caSecretName)
 		/*Configure CA*/
 		secretCAVolumeSpec := corev1.PodSpec{
 			Volumes: []corev1.Volume{
 				{
-					Name: caSecretName,
+					Name: volumeName,
 					VolumeSource: corev1.VolumeSource{
 						ConfigMap: &corev1.ConfigMapVolumeSource{
 							LocalObjectReference: corev1.LocalObjectReference{
@@ -96,7 +97,7 @@ func configureReceiversTLS(dep *v1.Deployment, caSecretName, certSecretName stri
 		secretCAContainerSpec := corev1.Container{
 			VolumeMounts: []corev1.VolumeMount{
 				{
-					Name:      caSecretName,
+					Name:      volumeName,
 					ReadOnly:  true,
 					MountPath: manifestutils.ReceiverTLSCADir,
 				},
@@ -110,11 +111,12 @@ func configureReceiversTLS(dep *v1.Deployment, caSecretName, certSecretName stri
 			return kverrors.Wrap(err, "failed to merge container")
 		}
 	}
+	secretVolumeName := naming.DNSName(certSecretName)
 
 	secretCertVolumeSpec := corev1.PodSpec{
 		Volumes: []corev1.Volume{
 			{
-				Name: certSecretName,
+				Name: secretVolumeName,
 				VolumeSource: corev1.VolumeSource{
 					Secret: &corev1.SecretVolumeSource{
 						SecretName: certSecretName,
@@ -126,7 +128,7 @@ func configureReceiversTLS(dep *v1.Deployment, caSecretName, certSecretName stri
 	secretCertContainerSpec := corev1.Container{
 		VolumeMounts: []corev1.VolumeMount{
 			{
-				Name:      certSecretName,
+				Name:      secretVolumeName,
 				ReadOnly:  true,
 				MountPath: manifestutils.ReceiverTLSCertDir,
 			},
