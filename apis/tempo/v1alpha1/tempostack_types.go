7840d11760a46c760529dea2d9927b0a2d1f9f04
Benedikt Bongartz <bongartz@klimlive.de>
2024-10-29 18:00:29 +0100

    switch to breaking change senario
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- apis/tempo/v1alpha1/tempostack_types.go
+++ apis/tempo/v1alpha1/tempostack_types.go
@@ -690,9 +690,10 @@ type JaegerQueryMonitor struct {
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Prometheus endpoint"
 	PrometheusEndpoint string `json:"prometheusEndpoint"`
 	// REDMetricsNamespace defines the a prefix used retrieve span rate, error, and duration (RED) metrics.
-	// By default no namespace is set.
+	// By default it is set to `traces.span.metrics` following the default namespace of the OpenTelemetry Collector since Version 0.109.0.
 	// +optional
 	// +kubebuilder:validation:Optional
+	// +kubebuilder:default:=traces.span.metrics
 	// +operator-sdk:csv:customresourcedefinitions:type=spec,displayName="RED Metric Namespace"
 	REDMetricsNamespace string `json:"redMetricsNamespace"`
 }
