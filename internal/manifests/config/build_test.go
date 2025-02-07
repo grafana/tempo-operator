febca5cc88111f55da76f9dfaf06ca347f3ef7e5
Ruben Vargas <ruben.vp8510@gmail.com>
2025-02-07 07:11:16 -0600

    Include insecure option and tls options when STS S3 token is enabled (#1108)
    * Include insecure option and tls options when STS S3 token is enabled
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    * Update internal/manifests/config/tempo-config.yaml
    
    Co-authored-by: Andreas Gerstmayr <andreas@gerstmayr.me>
    
    * update test
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    
    ---------
    
    Signed-off-by: Ruben Vargas <ruben.vp8510@gmail.com>
    Co-authored-by: Andreas Gerstmayr <andreas@gerstmayr.me>

--- internal/manifests/config/build_test.go
+++ internal/manifests/config/build_test.go
@@ -2528,6 +2528,7 @@ storage:
     s3:
       bucket: tempo
       endpoint: "s3.us-east-2.amazonaws.com"
+      insecure: true
     wal:
       path: /var/tempo/wal
 usage_report:
@@ -2560,6 +2561,124 @@ query_frontend:
 		},
 		StorageParams: manifestutils.StorageParams{
 			S3: &manifestutils.S3{
+				Insecure: true,
+				ShortLived: &manifestutils.S3ShortLived{
+					Bucket: "tempo",
+					Region: "us-east-2",
+				},
+			},
+		},
+		TLSProfile: tlsprofile.TLSProfileOptions{
+			MinTLSVersion: string(openshiftconfigv1.VersionTLS13),
+		},
+	})
+	require.NoError(t, err)
+	require.YAMLEq(t, expCfg, string(cfg))
+}
+
+func TestBuildConfiguration_S3_short_livedSecure(t *testing.T) {
+	expCfg := `
+---
+compactor:
+  compaction:
+    block_retention: 48h0m0s
+  ring:
+    kvstore:
+      store: memberlist
+distributor:
+  receivers:
+    jaeger:
+      protocols:
+        thrift_http:
+          endpoint: 0.0.0.0:14268
+        thrift_binary:
+          endpoint: 0.0.0.0:6832
+        thrift_compact:
+          endpoint: 0.0.0.0:6831
+        grpc:
+          endpoint: 0.0.0.0:14250
+    zipkin:
+        endpoint: 0.0.0.0:9411
+    otlp:
+      protocols:
+        grpc:
+          endpoint: "0.0.0.0:4317"
+        http:
+          endpoint: "0.0.0.0:4318"
+  ring:
+    kvstore:
+      store: memberlist
+ingester:
+  lifecycler:
+    ring:
+      kvstore:
+        store: memberlist
+      replication_factor: 1
+    tokens_file_path: /var/tempo/tokens.json
+  max_block_duration: 10m
+memberlist:
+  abort_if_cluster_join_fails: false
+  join_members:
+    - tempo-test-gossip-ring
+multitenancy_enabled: false
+querier:
+  max_concurrent_queries: 20
+  search:
+    external_hedge_requests_at: 8s
+    external_hedge_requests_up_to: 2
+  frontend_worker:
+    frontend_address: "tempo-test-query-frontend-discovery:9095"
+server:
+  grpc_server_max_recv_msg_size: 4194304
+  grpc_server_max_send_msg_size: 4194304
+  http_listen_port: 3200
+  http_server_read_timeout: 3m0s
+  http_server_write_timeout: 3m0s
+  log_format: logfmt
+storage:
+  trace:
+    backend: s3
+    blocklist_poll: 5m
+    cache: none
+    local:
+      path: /var/tempo/traces
+    s3:
+      bucket: tempo
+      endpoint: "s3.us-east-2.amazonaws.com"
+      insecure: false
+    wal:
+      path: /var/tempo/wal
+usage_report:
+  reporting_enabled: false
+query_frontend:
+  search:
+    concurrent_jobs: 2000
+    max_duration: 0s
+    max_spans_per_span_set: 0
+`
+	cfg, err := buildConfiguration(manifestutils.Params{
+		Tempo: v1alpha1.TempoStack{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test",
+			},
+			Spec: v1alpha1.TempoStackSpec{
+				Timeout: metav1.Duration{Duration: time.Minute * 3},
+				Storage: v1alpha1.ObjectStorageSpec{
+					Secret: v1alpha1.ObjectStorageSecretSpec{
+						Type: v1alpha1.ObjectStorageSecretS3,
+					},
+				},
+				ReplicationFactor: 1,
+				Retention: v1alpha1.RetentionSpec{
+					Global: v1alpha1.RetentionConfig{
+						Traces: metav1.Duration{Duration: 48 * time.Hour},
+					},
+				},
+			},
+		},
+		StorageParams: manifestutils.StorageParams{
+			S3: &manifestutils.S3{
+				Insecure: false,
 				ShortLived: &manifestutils.S3ShortLived{
 					Bucket: "tempo",
 					Region: "us-east-2",
