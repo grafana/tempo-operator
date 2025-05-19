a7f3ae9a8c62f94361c210e48d05a4fb32021602
Andreas Gerstmayr <agerstmayr@redhat.com>
2025-05-19 13:48:18 +0200

    [chore] add supported Kubernetes versions to changelog
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- hack/list-components.sh
+++ hack/list-components.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-TEMPO_VERSION=$(cat config/manager/manager.yaml | grep -oP "docker.io/grafana/tempo:\K.*")
+TEMPO_VERSION=$(grep -oP "docker.io/grafana/tempo:\K.*" config/manager/manager.yaml)
+KUBE_MIN_VERSION=$(grep -oP 'kube-version: "\K([\d.]*)' .github/workflows/e2e.yaml | sed -n '1p')
+KUBE_MAX_VERSION=$(grep -oP 'kube-version: "\K([\d.]*)' .github/workflows/e2e.yaml | sed -n '2p')
 
 cat << EOF
 ### Components
 - Tempo: [v${TEMPO_VERSION}](https://github.com/grafana/tempo/releases/tag/v${TEMPO_VERSION})
+
+### Support
+This release supports Kubernetes ${KUBE_MIN_VERSION} to ${KUBE_MAX_VERSION}.
 EOF
