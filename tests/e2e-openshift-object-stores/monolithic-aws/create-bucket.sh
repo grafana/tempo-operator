58e8b89ffafb0c6b8b7c3e84eff45ed245411b9a
Ishwar Kanse <ikanse@redhat.com>
2025-06-03 17:56:57 +0530

    Fetch AWS creds from CI if available

--- tests/e2e-openshift-object-stores/monolithic-aws/create-bucket.sh
+++ tests/e2e-openshift-object-stores/monolithic-aws/create-bucket.sh
@@ -5,17 +5,22 @@ BUCKET_NAME="ikanse-monolithic-s3"
 REGION="us-east-2"
 AWS_BUCKET_ENDPOINT="https://s3.${REGION}.amazonaws.com"
 
-# Fetch AWS credentials
-AWS_ACCESS_KEY_ID=$(oc get secret aws-creds -n kube-system -o json | jq -r '.data.aws_access_key_id' | base64 -d)
-if [ $? -ne 0 ]; then
-    echo "Failed to fetch AWS_ACCESS_KEY_ID"
-    exit 1
-fi
+# Fetch AWS credentials based on CLUSTER_PROFILE_DIR variable
+if [ -n "${CLUSTER_PROFILE_DIR}" ]; then
+    export AWS_ACCESS_KEY_ID=$(grep "aws_access_key_id=" "${CLUSTER_PROFILE_DIR}/.awscred" | cut -d '=' -f2)
+    export AWS_SECRET_ACCESS_KEY=$(grep "aws_secret_access_key=" "${CLUSTER_PROFILE_DIR}/.awscred" | cut -d '=' -f2)
+else
+    export AWS_ACCESS_KEY_ID=$(oc get secret aws-creds -n kube-system -o json | jq -r '.data.aws_access_key_id' | base64 -d)
+    if [ $? -ne 0 ]; then
+        echo "Failed to fetch AWS_ACCESS_KEY_ID"
+        exit 1
+    fi
 
-AWS_ACCESS_KEY_SECRET=$(oc get secret aws-creds -n kube-system -o json | jq -r '.data.aws_secret_access_key' | base64 -d)
-if [ $? -ne 0 ]; then
-    echo "Failed to fetch AWS_ACCESS_KEY_SECRET"
-    exit 1
+    export AWS_SECRET_ACCESS_KEY=$(oc get secret aws-creds -n kube-system -o json | jq -r '.data.aws_secret_access_key' | base64 -d)
+    if [ $? -ne 0 ]; then
+        echo "Failed to fetch AWS_SECRET_ACCESS_KEY"
+        exit 1
+    fi
 fi
 
 # Check if the bucket exists
@@ -46,7 +51,7 @@ kubectl -n $NAMESPACE create secret generic s3-secret \
   --from-literal=bucket="$BUCKET_NAME" \
   --from-literal=endpoint="$AWS_BUCKET_ENDPOINT" \
   --from-literal=access_key_id="$AWS_ACCESS_KEY_ID" \
-  --from-literal=access_key_secret="$AWS_ACCESS_KEY_SECRET"
+  --from-literal=access_key_secret="$AWS_SECRET_ACCESS_KEY"
 if [ $? -ne 0 ]; then
     echo "Failed to create secret"
     exit 1
