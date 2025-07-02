6aa3722f45e1e68bf81ef9fd9f96fdb45bba0e8a
Ishwar Kanse <ikanse@redhat.com>
2025-07-02 17:20:49 +0530

    Test Tempo instance metrics when multitenancy is enabled (#1230)
    Assisted by: Cursor IDE using claude-4-sonnet
    
    Co-authored-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- tests/e2e-openshift/monolithic-multitenancy-openshift/check_metrics.sh
+++ tests/e2e-openshift/monolithic-multitenancy-openshift/check_metrics.sh
@@ -6,8 +6,10 @@ oc adm policy add-cluster-role-to-user cluster-monitoring-view system:serviceacc
 TOKEN=$(oc create token e2e-test-metrics-reader -n $NAMESPACE)
 THANOS_QUERIER_HOST=$(oc get route thanos-querier -n openshift-monitoring -o json | jq -r '.spec.host')
 
-#Check metrics used in the prometheus rules created for TempoStack. Refer issue https://issues.redhat.com/browse/TRACING-3399 for skipped metrics.
-metrics="tempo_query_frontend_queries_total tempo_request_duration_seconds_count tempo_request_duration_seconds_sum tempo_request_duration_seconds_bucket tempo_build_info tempo_ingester_bytes_received_total tempo_ingester_flush_failed_retries_total tempo_ingester_failed_flushes_total tempo_ring_members tempo_operator_tempostack_managed tempo_operator_tempostack_storage_backend tempo_operator_tempostack_multi_tenancy"
+#Check TempoMonolithc metircs
+# Tempo component metrics not exposed due to bug. https://issues.redhat.com/browse/TRACING-5472
+#metrics="tempo_query_frontend_queries_total tempo_distributor_bytes_received_total tempo_distributor_spans_received_total tempo_ingester_bytes_received_total tempo_distributor_traces_per_batch_count tempo_build_info"
+metrics="http_request_duration_seconds_bucket"
 
 for metric in $metrics; do
 query="$metric"
@@ -25,4 +27,4 @@ while [[ $count -eq 0 ]]; do
     echo "Metric '$metric' with value is present."
     fi
   done
-done
+done 
