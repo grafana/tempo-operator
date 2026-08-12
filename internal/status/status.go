527c0c5263e915f167632beaa4be920d7f4c822c
Andreas Gerstmayr <agerstmayr@redhat.com>
2026-08-12 07:02:50 +0200

    feat: status condition should only be ready if pods are ready (#1582)
    * feat: status condition should only be ready if pods are ready
    
    Previously, the TempoStack status conditon was Ready if all pods were
    running, however the pod may have been not ready yet (i.e. readiness
    probe didn't succeed).
    
    The logic follows Loki Operator's status retrieval logic.
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * ignore linter
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * use JMESPath expression for Tempo readiness checks
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * add 5m timeout to Tempo readiness assert
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * re-try status updates, and fix dropping pods from status field
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * re-try status updates also for TempoMonolithic
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * debug chainsaw test
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * add PodSucceeded phase
    
    The compactor component may reach phase Succeeded.
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * skip terminating pods
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * improve condition
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    * add comment
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>
    
    ---------
    
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- internal/status/status.go
+++ internal/status/status.go
@@ -3,29 +3,46 @@ package status
 import (
 	"context"
 
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/util/retry"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	"github.com/grafana/tempo-operator/api/tempo/v1alpha1"
 	"github.com/grafana/tempo-operator/internal/version"
 )
 
 // Refresh updates the status field with the Tempo versions and updates the tempostack_status_condition metric.
 func Refresh(ctx context.Context, k StatusClient, tempo v1alpha1.TempoStack, status *v1alpha1.TempoStackStatus) error {
-	changed := tempo.DeepCopy()
-	changed.Status = *status
-
 	// The version fields in the status are empty for new CRs
 	if status.OperatorVersion == "" {
-		changed.Status.OperatorVersion = version.Get().OperatorVersion
+		status.OperatorVersion = version.Get().OperatorVersion
 	}
 	if status.TempoVersion == "" {
-		changed.Status.TempoVersion = version.Get().TempoVersion
+		status.TempoVersion = version.Get().TempoVersion
 	}
 
 	updateMetrics(metricTempoStackStatusCondition, status.Conditions, tempo.Namespace, tempo.Name)
 
-	err := k.PatchStatus(ctx, changed, &tempo)
-	if err != nil {
+	statusUpdater := func(tempo *v1alpha1.TempoStack) {
+		tempo.Status = *status
+	}
+
+	statusUpdater(&tempo)
+	// happy path: avoid extra k.Get()
+	// Use Update instead of Patch: a strategic merge patch merges maps and cannot remove keys, but PodStatusMap needs to drop entries for removed pods.
+	err := k.UpdateStatus(ctx, &tempo)
+	if err == nil || !errors.IsConflict(err) {
 		return err
 	}
 
-	return nil
+	// retry on conflict
+	objectKey := client.ObjectKeyFromObject(&tempo)
+	return retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		if err := k.Get(ctx, objectKey, &tempo); err != nil {
+			return err
+		}
+
+		statusUpdater(&tempo)
+		return k.UpdateStatus(ctx, &tempo)
+	})
 }
