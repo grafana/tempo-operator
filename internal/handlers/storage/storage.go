940117cfa43f6f88cb680ae35e4b2c27132dda53
Andreas Gerstmayr <agerstmayr@redhat.com>
2026-09-24 11:18:39 +0200

    fix: for S3 static credentials, derive the HTTP/HTTPS transport from the storage secret endpoint scheme instead of the storage TLS toggle (#1649)
    Signed-off-by: Andreas Gerstmayr <agerstmayr@redhat.com>

--- internal/handlers/storage/storage.go
+++ internal/handlers/storage/storage.go
@@ -60,21 +60,20 @@ func GetStorageParamsForTempoStack(ctx context.Context, client client.Client, te
 			}
 			storageParams.CloudCredentials.ContentHash = contentHash
 		}
+
+		// getS3Params() sets S3.Insecure:
+		// - for static credentials, S3.Insecure is derived from the endpoint scheme (http:// means insecure)
+		// - for token-based credentials, S3.Insecure is false (TLS is always enabled)
 		storageParams.S3, errs = getS3Params(storageSecret, secretNamePath, credentialMode)
 
 		if len(errs) > 0 {
 			return manifestutils.StorageParams{}, errs
 		}
 
-		// Token-based auth (STS/IRSA) requires HTTPS to communicate with AWS endpoints.
-		// Only fall back to the user-controlled TLS toggle for static credentials.
-		if credentialMode == v1alpha1.CredentialModeToken || credentialMode == v1alpha1.CredentialModeTokenCCO {
-			storageParams.S3.Insecure = false
-		} else {
-			storageParams.S3.Insecure = !tempo.Spec.Storage.TLS.Enabled
-		}
-
 		if tempo.Spec.Storage.TLS.Enabled {
+			// Enabling TLS forces the connection to object storage over HTTPS.
+			storageParams.S3.Insecure = false
+
 			storageParams.S3.TLS, errs = getTLSParams(ctx, client, tempo.Namespace, tempo.Spec.Storage.TLS, tlsPath.Child("caName"))
 			if len(errs) > 0 {
 				return manifestutils.StorageParams{}, errs
@@ -221,6 +220,9 @@ func GetStorageParamsForTempoMonolithic(ctx context.Context, client client.Clien
 			storageParams.CloudCredentials.ContentHash = contentHash
 		}
 
+		// getS3Params() sets S3.Insecure:
+		// - for static credentials, S3.Insecure is derived from the endpoint scheme (http:// means insecure)
+		// - for token-based credentials, S3.Insecure is false (TLS is always enabled)
 		storageParams.S3, errs = getS3Params(storageSecret, secretNamePath, credentialMode)
 
 		if len(errs) > 0 {
@@ -228,6 +230,9 @@ func GetStorageParamsForTempoMonolithic(ctx context.Context, client client.Clien
 		}
 
 		if tempo.Spec.Storage.Traces.S3.TLS != nil && tempo.Spec.Storage.Traces.S3.TLS.Enabled {
+			// Enabling TLS forces the connection to object storage over HTTPS.
+			storageParams.S3.Insecure = false
+
 			caPath := tracesPath.Child("s3", "tls", "caName")
 			storageParams.S3.TLS, errs = getTLSParams(ctx, client, tempo.Namespace, *tempo.Spec.Storage.Traces.S3.TLS, caPath)
 			if len(errs) > 0 {
